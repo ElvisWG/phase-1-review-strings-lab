21cfde573d4fa77fbebcad3ef383a5f6ea2935d4
Elvis Wanjiru <elvis.gatukui@student.moringaschool.com>
2022-08-21 15:11:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = 'Welcome to Flatbook, ${Grace Hopper}!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase(welcomeMessage);
+const shortGreeting ='Welcome, ${G}!';
